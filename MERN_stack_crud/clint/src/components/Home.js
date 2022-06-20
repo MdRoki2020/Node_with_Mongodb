22eefb62235a5fc753f18101300b66bd2d937873
MdRoki2020 <mroki815@gmail.com>
2022-06-20 11:14:26 +0600

    data fetch done

--- MERN_stack_crud/clint/src/components/Home.js
+++ MERN_stack_crud/clint/src/components/Home.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
-import { Form } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react'
+import { Form,Table } from 'react-bootstrap';
 import Axios from 'axios';
 
 export default function Home() {
 
     const [name,setName]=useState('');
     const [age,setAge]=useState(0);
+    const [listOfFriends,setListOfFriends]=useState([]);
 
     const addFriend=()=>{
-        Axios.post('http://localhost:3001/addfriend',{name:name,age:age}).then(()=>{
-            alert("yey, its work");
-        }).catch(()=>{
-            alert("aww, it didnt work");
-        });
+        Axios.post('http://localhost:3001/addfriend',{
+            name:name,
+            age:age,
+        })
     }
+
+    useEffect(()=>{
+        Axios.get("http://localhost:3001/read").then((response)=>{
+            setListOfFriends(response.data);
+        }).catch(()=>{
+            console.log("Error !");
+        })
+    },[]);
 
   return (
     <div className='container'>
@@ -36,6 +44,37 @@ export default function Home() {
             </div>
             <div className='col-md-4'></div>
             <div className='col-md-4'></div>
+        </div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-3'></div>
+                <div className='col-md-6'>
+        <Table className='striped bordered hover'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>AGE</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          {
+              listOfFriends.map((value)=>
+              <tr key={value._id}>
+                <td>{value._id}</td>
+                <td>{value.name}</td>
+                <td>{value.age}</td>
+                {/* <td><Link to={"/contactUpdate/"+value.id}><button className='btn btn-info'>Edit</button></Link> <button onClick={()=>{deleteData(value.id)}} className='btn btn-danger'>Delete</button></td> */}
+              </tr>
+              )
+          }
+
+          </tbody>
+        </Table>
+                </div>
+                <div className='col-md-3'></div>
+            </div>
         </div>
     </div>
   )
